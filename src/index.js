a9bb4ca70b55f97328415054ece31a5ddfc51fa1
agustinkra <166574046+agustinkra@users.noreply.github.com>
2024-05-14 23:12:46 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import  {ProductManager}  from './productManager.js';
+import  {productsRouter}  from './routes/products.router.js';
+
+const PORT = 8080;
+
+const app = express();
+
+export const productManager = new ProductManager;
+
+app.use(express.json())
+app.use ('/api/products', productsRouter) // http://localhost:PORT/api/products
+
+app.listen(PORT, (req, res) => {
+     console.log(`Servidor escuchando en el puerto ${PORT}`);
+})
