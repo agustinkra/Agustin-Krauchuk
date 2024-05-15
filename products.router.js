b4da328e1d5608e0482acf0ed4c575ff0509a2ec
agustinkra <166574046+agustinkra@users.noreply.github.com>
2024-05-14 22:56:45 -0300

    Add files via upload

---
+++ products.router.js
@@ -0,0 +1,80 @@
+import { Router } from 'express';
+import ProductManager from '../productManager.js';
+
+const productsRouter = Router()
+
+
+// http://localhost:8080/products
+
+
+productsRouter.get('/', async(req, res)=>{
+try {
+    const {limit} = req.query;
+    const products = ProductManager.getProducts()
+
+    if(limit){
+        const LimitedProducts = products.slice(0, limit)
+        return res.json(LimitedProducts) 
+    }
+
+    return res.json (products)
+
+} catch (error) {
+    console.log(error);
+    res.send('ERROR AL INTENTAR RECIBIR LOS PRODUCTOS')
+}
+})
+
+productsRouter.get('/:pid', async (req, res)=>{
+try {
+    const {pid} = req.params;
+    const products = ProductManager.getProductById(pid)
+    res.json(products)
+} catch (error) {
+    console.log(error);
+    res.send(`ERROR AL INTENTAR RECIBIR EL PRODUCTO CON ID ${pid}`)
+}
+})
+
+productsRouter.post('/', async (req, res)=>{
+    try {
+        const {title, description, price, thumbnail, code, stock, status, category} = req.body;
+
+        const response = await ProductManager.addProduct({title, description, price, thumbnail, code, stock, status, category})
+        res.json (response)
+    } catch (error) {
+        console.log(error);
+        res.send(`ERROR AL INTENTAR AGREGAR PRODUCTO`)
+    }
+})
+
+
+
+
+productsRouter.put('/:pid', async (req, res)=> {
+    const {pid} = req.params;
+
+    try {
+        const {title, description, price, thumbnail, code, stock, status, category} = req.body;
+        const response = await ProductManager.updateProduct(id, {title, description, price, thumbnail, code, stock, status, category})
+        res.json(response)
+    } catch (error) {
+        console.log(error);
+        res.send(`ERROR AL INTENTAR EDITAR PRODUCTO CON ID ${pid}`)
+    }
+})
+
+
+productsRouter.delete('/:pid', async (req, res)=>{
+    const {pid} = req.params;
+    try {
+        await ProductManager.deleteProduct(id)
+        res.send('PRODUCTO ELIMINADO EXITOSAMENTE')
+    } catch (error) {
+        console.log(error);
+        res.send(`ERROR AL INTENTAR ELIMINAR PRODUCTO CON ID ${pid}`)
+    }
+})
+
+
+export default productsRouter
