e950f0a228b7549a9c97362500dae6576cc47651
agustinkra <166574046+agustinkra@users.noreply.github.com>
2024-05-29 21:13:35 -0300

    segunda pre entrega

---
+++ public/js/index.js
@@ -0,0 +1,58 @@
+
+const socket = io();
+
+socket.on("products",  (products) => {
+    try {
+        console.log("recibido evento products");
+        const productCards = products.map((product) => {
+            return `
+                <div class="card">
+                    <h2>${product.title}</h2>
+                    <p>${product.description}</p>
+                    <div><p>$${product.price} - Stock: ${product.stock}</p></div>
+                    <button class="delete-btn" data-id="${product.id}">Eliminar</button>
+                </div>
+            `;
+        }).join(" ");
+
+        document.getElementById("card-container").innerHTML = productCards;
+
+        // botones para eliminar -.-"
+
+        document.querySelectorAll('.delete-btn').forEach(button => {
+            button.addEventListener('click', (event) => {
+                const productId = event.target.getAttribute('data-id');
+                eliminarProducto(productId);
+            });
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+// form para agregar producto
+document.getElementById('form').addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    
+    const newProduct = {
+        title: document.getElementById('title').value,
+        description: document.getElementById('description').value,
+        category: document.getElementById('category').value,
+        code: document.getElementById('code').value,
+        price: parseFloat(document.getElementById('price').value),
+        stock: parseInt(document.getElementById('stock').value, 10)
+    }
+
+    
+
+    socket.emit('newProduct', newProduct);
+
+    document.getElementById('form').reset();
+});
+
+// función eliminar un producto
+const eliminarProducto =  (id) => {
+    console.log("evento deleteProduct enviado");
+    socket.emit('deleteProduct', id);
+}
