e2e6a9bcdc5d9a0bc6f93f285b8a07af92177f8b
agustinkra <166574046+agustinkra@users.noreply.github.com>
2024-06-20 20:39:58 -0300

    Add files via upload

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -1,11 +1,83 @@
 import { Router } from "express";
 import { __dirname } from "../path.js";
-import ProductManager from "../managers/products.manager.js";
+import * as productService from "../services/product.services.js";
+import * as cartService from "../services/cart.services.js";
+
 
 const router = Router();
-const productManager = new ProductManager(`${__dirname}/db/products.json`);
 
-router.get("/", async (req, res) => {
+const generateLink = (url, page, limit, title, sort) => {
+  let link = `${url}?page=${page}`;
+  if (limit) link += `&limit=${limit}`;
+  if (title) link += `&title=${title}`;
+  if (sort) link += `&sort=${sort}`;
+  return link;
+};
+
+router.get("/",  async (req, res, next) => {
+  try {
+    const { page = 1, limit = 10, title, sort } = req.query;
+    const response = await productService.getAllProducts(page, limit, title, sort);
+    const products = response.docs ? response.docs : [];
+    const url = 'http://localhost:8080/';
+    const nextLink = response.hasNextPage ? generateLink(url, response.nextPage, limit, title, sort) : null;
+    const prevLink = response.hasPrevPage ? generateLink(url, response.prevPage, limit, title, sort) : null;
+
+    res.render("home", {
+      products,
+      page: response.page,
+      hasNextPage: response.hasNextPage,
+      hasPrevPage: response.hasPrevPage,
+      nextLink,
+      prevLink
+    });
+  } catch (error) {
+    console.log("error al renderizar ❌");
+    next(error.message);
+  }
+});
+
+router.get("/product/:pid", async (req, res, next) => {
+  try {
+    const { pid } = req.params;
+    const product = await productService.getProductById(pid);
+    if (!product) {
+      return res.status(404).render("error", { message: "Producto no encontrado" });
+    }
+    res.render("product", { product });
+  } catch (error) {
+    console.log("error al obtener el producto ❌");
+    next(error.message);
+  }
+});
+
+router.post("/carts/:cid/product/:pid", async (req, res, next) => {
+  try{
+    const { cid } = req.params;
+    const { pid } = req.params;
+    const product = await service.addProductToCart(cid,pid) 
+    res.render("product", { product });
+  } catch (error) {
+    console.log("error al agregar el producto ❌");
+    next(error.message);
+  }
+});
+
+router.get("/carts/:cid", async (req, res, next) => {
+  try {
+    const { cid } = req.params;
+    const cart = await cartService.getCartById(cid);
+    if (!cart) {
+      return res.status(404).render("error", { message: "Carrito no encontrado" });
+    }
+    res.render("cart", { cart });
+  } catch (error) {
+    console.log("error al obtener el Carrito ❌");
+    next(error.message);
+  }
+});
+
+router.get("/old", async (req, res) => {
   try {
     const { limit } = req.query;
     const products = await productManager.getProducts(limit);
@@ -15,8 +87,8 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/realtimeproducts", async (req, res) => {
-  res.render("realtimeproducts");
+router.get("/admin", async (req, res) => {
+  res.render("admin");
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { __dirname } from "./src/path.js";
-import handlebars from 'express-handlebars';
-import { Server } from "socket.io";
+import hbs from "./src/handlebarsHbs.js";
+
 
 import viewsRouter from './src/routes/views.routes.js';
 import cartRouter from './src/routes/carts.routes.js';
 import productsRouter from './src/routes/products.routes.js';
-import ProductsManager from "./src/managers/products.manager.js";
+import { errorHandler } from "./src/middlewares/errorHandler.js";
+import { initMongoDB }  from './src/db/mongoDb.js';
 
 
 
-const productManager = new ProductsManager(`${__dirname}/db/products.json`);
+
 const app = express();
 
 app.use(express.json());
@@ -21,39 +22,16 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/carts', cartRouter);
 app.use('/api/products', productsRouter);
 
-app.engine('handlebars', handlebars.engine());
+app.engine('handlebars', hbs.engine);
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'handlebars');
 
 app.use('/', viewsRouter);
 
+app.use(errorHandler);
+
+initMongoDB();
+
 const PORT = 8080;
 
-const httpServer = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const socketServer = new Server(httpServer);
-
-socketServer.on('connection', async (socket) => {
-    console.log(`New client connected - client ID: ${socket.id}`);
-    
-    socket.emit('products', await productManager.getProducts());
-    console.log("Products sent to client");
-
-    socket.on('disconnect', () => console.log(`Client disconnected`));
-
-    socket.on('newProduct', async (newProduct) => {
-
-        productManager.addNewProduct(newProduct);
-        const products = await productManager.getProducts();
-        socketServer.emit('products', products);
-    });
-
-    socket.on('deleteProduct', async (id) => {
-        await productManager.deleteProduct(id);
-        console.log("Product deleted");
-        const products = await productManager.getProducts();
-        socketServer.emit('products', products);
-    });
-
-
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
